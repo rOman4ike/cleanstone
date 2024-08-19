8f1c262329e50c759bb9d4a995dc53046325bc95
rOman4ike <rOman4ike@yandex.ru>
2024-08-20 02:20:32 +0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+cart = Cart.create()
+
+products = Product.create([
+  {
+    name: 'Беспроводная колонка Goodyear Bluetooth Speaker',
+    price: rand(800..2000)
+  },
+  {
+    name: 'Женский домашний костюм Sweet Dreams',
+    price: rand(800..2000)
+  },
+  {
+    name: 'Плащ-дождевик SwissOak',
+    price: rand(800..2000)
+  }
+])
+
+products.each_with_index do |product, index|
+  image_name = "image-#{index + 1}.png"
+  product.image.attach(
+    io: File.open(Rails.root.join("db/images/#{image_name}")),
+    filename: image_name
+  )
+  product.save
+  CartItem.create(cart_id: cart.id, product_id: product.id)
+end
