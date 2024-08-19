7315e23b9634b624c07cd6158b1c4176fa3a61ab
rOman4ike <rOman4ike@yandex.ru>
2024-08-20 02:24:40 +0700

    configurerd tailwind && added base styles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -8,6 +9,27 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      ...colors,
+      'primary': {
+        500: '#4AD9BD',
+        700: '#41C1A9'
+      },
+      'light': {
+        'gray': {
+          100: '#F8F8F9',
+          300: '#ECEFF4',
+          500: '#8A93A3'
+        }
+      },
+      'dark': {
+        'gray': {
+          700: '#8A93A3',
+          800: '#515767',
+          900: '#1E1E3C'
+        }
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
