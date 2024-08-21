49540a0887932d51af984c14745f490b4e89c002
rOman4ike <rOman4ike@yandex.ru>
2024-08-21 09:31:00 +0700

    added helper method (convert number to ru currency)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  include ActionView::Helpers::NumberHelper
+  helper_method :number_to_RU_currency
+
+  def number_to_RU_currency(number)
+    number_to_currency(number,
+      unit: '₽',
+      separator: "",
+      delimiter: " ",
+      format: "%n %u",
+      precision: 0
+    )
+  end
 end
