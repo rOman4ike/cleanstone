1935f245447b946d8703bbca15ac6543c612afb7
rOman4ike <rOman4ike@yandex.ru>
2024-08-21 09:35:58 +0700

    added discount input range logic

---
+++ app/javascript/controllers/discount_controller.js
@@ -0,0 +1,39 @@
+import ApplicationController from "./application_controller"
+import { FetchRequest } from '@rails/request.js'
+
+export default class extends ApplicationController {
+  connect() {
+    this.cartId = this.element.dataset.cartId
+    this.totalPrice = this.element.dataset.totalPrice
+    this.$discountBlock = document.querySelector('.js-discount--value')
+    this.$finalPriceBlock = document.querySelector('.js-discount--final-price')
+  }
+
+  #numberToRUCurrency(value) {
+    return Intl.NumberFormat('ru-RU', {
+      style: 'currency',
+      currency: 'RUB',
+      minimumFractionDigits: 0,
+    }).format(value)
+  }
+
+  async #sendRequest(params, discountValue) {
+    const request = new FetchRequest('patch', `/carts/${this.cartId}`, { body: params })
+    const response = await request.perform()
+    if (response.ok) {
+      const finalPrice = Math.max(this.totalPrice - discountValue, 0)
+      this.$discountBlock.innerHTML = this.#numberToRUCurrency(discountValue)
+      this.$finalPriceBlock.innerHTML = this.#numberToRUCurrency(finalPrice)
+    }
+  }
+
+  onChange(event) {
+    const discountValue = event.target.value
+    const params = JSON.stringify({
+      cart: {
+        discount: discountValue
+      }
+    })
+    this.#sendRequest(params, discountValue)
+  }
+}
